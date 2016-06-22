54425ad2654028ef5d692458841d196e0b82071a
Nguyen Thi Thu Ha D <nguyen.thi.thu.had@gmail.com>
2016-06-22 12:10:24 +0700

    create signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :activites, dependent: :destroy
   has_many :lessons, dependent: :destroy
   has_many :active_relationships, class_name:  Relationship.name,
@@ -9,5 +11,13 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
+
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   get "help"    => "static_pages#help"
   get "about"   => "static_pages#about"
   get "contact" => "static_pages#contact"
+
   get "login"  => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
+
+  get "signup"  => "users#new"
+  resources :users
+
 end
